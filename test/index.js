85acf2f4072ff98f634a724451f27f9742a22b64
Arun <arunkumarit02@gmail.com>
2019-04-20 01:24:53 +0530

    testing route

---
+++ test/index.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+// let chaiHttp = require('chai-http');
+
+describe('/GET test', () => {
+    it('it should passed', (done) => {
+        done();
+    });
+});
